650e2122e62d0aacf6b9132ebb203b0b97e4e104
den-a96 <den-a96@mail.ru>
2024-06-09 11:45:21 +0300

    add branch feature/di-annotation

--- 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ 04_serlvets/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,44 +1,17 @@
 package ru.netology.repository;
 
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.Collection;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-// Stub
-public class PostRepository {
-    private final ConcurrentMap<Long, Post> storagePosts = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger();
+public interface PostRepository {
 
-    public Collection<Post> all() {
-        return storagePosts.values();
-    }
+    Collection<Post> all();
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(storagePosts.get(id));
-    }
+    Optional<Post> getById(long id);
 
-    public Post save(Post post) {
-        if (post.getId() != 0) {
-            Optional<Post> currentPost = getById(post.getId());
-            if (currentPost.isEmpty()) {
-                throw new NotFoundException("Post with id " + post.getId() + " not found");
-            } else {
-                currentPost.get().setContent(post.getContent());
-                return currentPost.get();
-            }
-        } else {
-            Post newPost = new Post(counter.incrementAndGet(), post.getContent());
-            storagePosts.put(newPost.getId(), newPost);
-            return newPost;
-        }
-    }
+    Post save(Post post);
 
-    public void removeById(long id) {
-        storagePosts.remove(id);
-    }
+    void removeById(long id);
 }
